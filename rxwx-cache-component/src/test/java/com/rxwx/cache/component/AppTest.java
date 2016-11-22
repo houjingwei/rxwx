7b0feb404a1ec4fc967be998d31711906fa3607e
jingwei Hou <hjw@jingweideMacBook-Pro.local>
2016-11-22 14:49:14 +0800

    cache test

--- rxwx-cache-component/src/test/java/com/rxwx/cache/component/AppTest.java
+++ rxwx-cache-component/src/test/java/com/rxwx/cache/component/AppTest.java
@@ -1,6 +1,7 @@
 package com.rxwx.cache.component;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.junit.Test;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.rxwx.common.utils.DateUtil;
 import com.rxwx.model.User;
 import com.rxwx.service.cache.CacheService;
 
@@ -21,17 +23,30 @@ public class AppTest extends TestCase {
 	@Autowired CacheService cacheService;
 	@Test
     public void cacheTest(){  
-    	User user = new User();
-    	user.setUsername("dfgg");
-    	cacheService.add("user1", user, 1);
-    	User user2 =  (User) cacheService.get("user1");
-    	System.out.println(user2.getUsername());
+		List<User> userList = new ArrayList<User>();
+    	for(int i=0 ;i<20000;i++){
+    		User user = new User();
+    		user.setEmail("houjingwei"+i+"@163.com");
+    		user.setPassword("password"+i);
+    		user.setUsername("houjingwei"+i);
+    		userList.add(user);
+    		
+    	}
+    	Date startSaveDate = new Date();
+    	System.out.println("开始缓存"+DateUtil.convert2String(startSaveDate, "yyyy-MM-dd HH:mm:ss"));
     	
+    	cacheService.add("userList", userList);
     	
-    	List<String> ll = new ArrayList<String>();
-    	ll.add("1");
-    	ll.add("2");
-    	cacheService.getList("llyy");
-    	System.out.println(cacheService.getList("llyy"));
+    	Date endSaveDate = new Date();
+    	System.out.println("缓存结束"+DateUtil.convert2String(endSaveDate, "yyyy-MM-dd HH:mm:ss"));
+    	System.out.println("存储对象集合耗时<毫秒>:"+DateUtil.diffMs(startSaveDate, endSaveDate)+"ms");
+    	
+    	Date startGetDate = new Date();
+    	System.out.println("开始读取"+DateUtil.convert2String(startGetDate, "yyyy-MM-dd HH:mm:ss"));
+    	Object obj = cacheService.get("userList");
+    	List<User> users = (List<User>) obj;
+    	Date endGetDate = new Date();
+    	System.out.println("读取结束"+DateUtil.convert2String(endGetDate, "yyyy-MM-dd HH:mm:ss"));
+    	System.out.println("读取对象集合耗时<毫秒>:"+DateUtil.diffMs(startGetDate, endGetDate)+"ms");
     } 
 }
