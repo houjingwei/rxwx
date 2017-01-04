a8b1b9f3bee40e348a4542f2ba2e28998a1d562e
houjingwei@163.com <meky1978>
2017-01-04 09:53:13 +0800

    fuck

---
+++ rxwx-admin/src/main/java/com/rxwx/admin/shiro/ShiroSessionUtils.java
@@ -0,0 +1,59 @@
+package com.rxwx.admin.shiro;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
+
+import com.rxwx.model.Admin;
+
+public class ShiroSessionUtils {
+	public static final String LOGIN_ATTRIVUTE_NAME = "login_admin_user";
+
+	public static void setAttribute(Object key, Object value) {
+		Subject account = SecurityUtils.getSubject();
+		if (null != account) {
+			Session session = account.getSession();
+			if (null != session) {
+				session.setAttribute(key, value);
+			}
+		}
+	}
+
+	public static Object getAttribute(Object key) {
+		Subject account = SecurityUtils.getSubject();
+		if (null != account) {
+			Session session = account.getSession();
+			if (null != session) {
+				return session.getAttribute(key);
+			}
+		}
+		return null;
+	}
+
+	public static Object removeAttribute(Object key) {
+		Subject account = SecurityUtils.getSubject();
+		if (null != account) {
+			Session session = account.getSession();
+			if (null != session) {
+				return session.removeAttribute(key);
+			}
+		}
+		return null;
+	}
+
+	public boolean hasLogin() {
+		return getAttribute(LOGIN_ATTRIVUTE_NAME) != null ? true : false;
+	}
+
+	public static void setAsLogin(Admin admin) {
+		setAttribute(LOGIN_ATTRIVUTE_NAME, admin);
+	}
+
+	public static void setAsLogout() {
+		removeAttribute(LOGIN_ATTRIVUTE_NAME);
+	}
+
+	public static Admin getLoginAccount() {
+		return (Admin) getAttribute(LOGIN_ATTRIVUTE_NAME);
+	}
+}
